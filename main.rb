e46400c30385d5230c37dbf6d561fe628025797c
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-10-26 12:35:27 +0200

    feat: Load hosts from config file

--- main.rb
+++ main.rb
@@ -19,9 +19,13 @@ def kubectl_get_pg_service(namespace, decidim_name)
   }
 end
 
-puts "Hello World!"
 puts "Airbyte ruby version: #{AirbyteRuby::VERSION}"
 
+new_hosts = YAML.load_file("config.yml")&.fetch("hosts", [])
+unless new_hosts.size.positive?
+  puts "No hosts requested, end of process !"
+  return
+end
 
 puts "Looking for Kubernetes host..."
 syscmd = Lib::Kubectl.get_hosts
@@ -36,59 +40,68 @@ end
 hosts = JSON.parse(syscmd.stdout)&.fetch("items", [])
 hosts = hosts.map { |host| Lib::KubeHost.from_hash(host) }
 puts "Found #{hosts.count} hosts!"
-target = hosts.select { |format| format.host == "decidim-migration-dry-run.osp.dev" }.first
+targets = hosts.select { |format| new_hosts.include?(format.host) }
 
-pg_credentials = Lib::Postgres.from_hash(Lib::Kubectl.get_pg_credentials(target.namespace, target.decidim_name))
-exists_service = Lib::Kubectl.get_pg_service(target.namespace, target.decidim_name).status.success?
-
-AirbyteRuby::Configuration.tap do |c|
-  c.endpoint = ENV.fetch("AIRBYTE_ENDPOINT", "http://localhost:8006/")
-  c.basic_auth = ENV.fetch("AIRBYTE_BASIC_AUTH", "true") == "true"
-  c.airbyte_username = ENV.fetch("AIRBYTE_USERNAME", "airbyte")
-  c.airbyte_password = ENV.fetch("AIRBYTE_PASSWORD", "password")
+if targets.size != new_hosts.size
+  puts "Some hosts were not found!"
+  puts "Not found hosts: #{new_hosts - targets.map(&:host)}"
 end
 
+targets.each do |target|
+  puts "Processing '#{target.host}'..."
+  pg_credentials = Lib::Postgres.from_hash(Lib::Kubectl.get_pg_credentials(target.namespace, target.decidim_name))
 
-db_host = ENV["DB_HOST"] || "#{target.decidim_name}--de-pg-repl-headless.#{target.namespace}.svc.cluster.local"
-db_port = ENV["DB_PORT"]&.to_i || 5432
-db_name = ENV["DB_NAME"] || "decidim"
-workspace_uuid = ENV["WORKSPACE_UUID"]
+  # exists_service = Lib::Kubectl.get_pg_service(target.namespace, target.decidim_name).status.success?
 
-postgres_adapter = AirbyteRuby::Adapters::Postgres.new(
-  host: db_host,
-  port: db_port,
-  database: db_name,
-  username: pg_credentials.username,
-  password: pg_credentials.password,
-  schema: "public",
-  ssl_mode: { mode: "prefer" },
-  replication_method: { method: "Xmin" },
-  tunnel_method: { tunnel_method: "NO_TUNNEL" }
-)
+  AirbyteRuby::Configuration.tap do |c|
+    c.endpoint = ENV.fetch("AIRBYTE_ENDPOINT", "http://localhost:8006/")
+    c.basic_auth = ENV.fetch("AIRBYTE_BASIC_AUTH", "true") == "true"
+    c.airbyte_username = ENV.fetch("AIRBYTE_USERNAME", "airbyte")
+    c.airbyte_password = ENV.fetch("AIRBYTE_PASSWORD", "password")
+  end
 
-airbyte_source = AirbyteRuby::Resources::Source.new(
-  postgres_adapter,
-  name: "[Decidim] Dry run migration",
-  workspace_id: workspace_uuid
-)
 
-res = airbyte_source.new
+  db_host = ENV["DB_HOST"] || "#{target.decidim_name}--de-pg-repl-headless.#{target.namespace}.svc.cluster.local"
+  db_port = ENV["DB_PORT"]&.to_i || 5432
+  db_name = ENV["DB_NAME"] || "decidim"
+  workspace_uuid = ENV["WORKSPACE_UUID"]
 
-if res.success?
-  params = JSON.parse(res.body)
-  puts "
-  Source successfully created!
+  postgres_adapter = AirbyteRuby::Adapters::Postgres.new(
+    host: db_host,
+    port: db_port,
+    database: db_name,
+    username: pg_credentials.username,
+    password: pg_credentials.password,
+    schema: "public",
+    ssl_mode: { mode: "prefer" },
+    replication_method: { method: "Xmin" },
+    tunnel_method: { tunnel_method: "NO_TUNNEL" }
+  )
 
-  Action : Create
-  Airbyte response : #{res.status}
-  Source UUID: #{params["sourceId"]}
-  Source Name: #{params["name"]}
+  airbyte_source = AirbyteRuby::Resources::Source.new(
+    postgres_adapter,
+    name: "[Decidim] #{target.decidim_name} - #{target.host}",
+    workspace_id: workspace_uuid
+  )
 
-  See more on http://localhost:8000/workspaces/#{workspace_uuid}/source/#{params["sourceId"]}
-       "
-else
-  puts "Something went wrong : HTTP Status: #{res.status}"
-  puts "#{res.body}"
+  res = airbyte_source.new
+
+  if res.success?
+    params = JSON.parse(res.body)
+    puts "
+    Source successfully created!
+
+    Action : Create
+    Airbyte response : #{res.status}
+    Source UUID: #{params["sourceId"]}
+    Source Name: #{params["name"]}
+
+    See more on #{ENV.fetch("AIRBYTE_BASE_URL", "http://localhost:8000/")}workspaces/#{workspace_uuid}/source/#{params["sourceId"]}
+  "
+  else
+    puts "Something went wrong : HTTP Status: #{res.status}"
+    puts "#{res.body}"
+  end
+
+  puts "Created source '#{airbyte_source.name}' !"
 end
-
-puts "Created source '#{airbyte_source.name}' !"
